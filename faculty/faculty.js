41523804785e7ac451023b143cb62b08bd8240b5
Nic Chaufournier <rednic11@gmail.com>
2015-04-04 11:21:57 -0400

    Changed Add Button on the form
    Made the form more dynamin

--- faculty/faculty.js
+++ faculty/faculty.js
@@ -1,10 +1,10 @@
 function addObjective(){
-	var element = document.createElement("input");
+	var element = document.createElement("INPUT");
 	
 	element.setAttribute("type", "text");
 	element.setAttribute("value", "");
 	element.setAttribute("name", "Test Name");
 	
 	var foo = document.getElementById("objectivesDiv");
-	foo.appendChild(element);
+	document.foo.appendChild(element);
 }
