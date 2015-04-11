4339549376bf0e196e9e1f19f6184a351ddd9604
Nic Chaufournier <rednic11@gmail.com>
2015-04-10 20:29:24 -0400

    Worked on Database php class
    Tried to get the database connection working.  No luck yet.

--- database/Database.php
+++ database/Database.php
@@ -3,12 +3,12 @@ class Database {
 	protected $_link, $_result, $_numRows;
 	
 	public function __construct($server, $username, $password, $db){
-		$this->_link = mysql_connect($server, $username, $password);
-		mysql_select_db($dbm $this->_link);
+		$this->_link = mysqli_connect($server, $username, $password);
+		mysql_select_db($db, $this->_link);
 	}
 	
 	public function disconnect() {
-		mysql_close($this _link);
+		mysql_close($this->_link);
 	}
 	
 	public function query($sql){
